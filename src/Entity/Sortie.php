0d38c8f119b68eb467f894c3c35e42cdaa43c399
HoussSak <99645587+HoussSak@users.noreply.github.com>
2023-10-09 15:15:43 +0200

    adding bootstrap

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SortieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SortieRepository::class)]
+class Sortie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private $id;
+
+    #[ORM\Column(type: "string", length: 30)]
+    private $nom;
+
+    #[ORM\Column(type: "datetime")]
+    private $dateDebut;
+
+    #[ORM\Column(type: "integer", nullable: true)]
+    private $duree;
+
+    #[ORM\Column(type: "datetime")]
+    private $dateCloture;
+
+    #[ORM\Column(type: "text")]
+    private $descriptioninfos;
+
+    #[ORM\Column(type: "integer")]
+    private $nbInscriptionsMax;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private $urlPhoto;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: "sortiesOrganisees")]
+    #[ORM\JoinColumn(nullable: false)]
+    private $auteur;
+
+    #[ORM\ManyToOne(targetEntity: Etat::class, inversedBy: "sorties")]
+    #[ORM\JoinColumn(nullable: false)]
+    private $etat;
+
+    #[ORM\ManyToOne(targetEntity: Lieu::class, inversedBy: "sorties")]
+    #[ORM\JoinColumn(nullable: false)]
+    private $lieu;
+
+    #[ORM\ManyToOne(targetEntity: Site::class, inversedBy: "sorties")]
+    private $site;
+
+    #[ORM\ManyToMany(targetEntity: User::class, fetch: "EAGER", mappedBy: "inscriptions")]
+    private $usersInscrits;
+
+    #[ORM\Column(type: "text", nullable: true)]
+    private $motifAnnulation;
+
+    public function __construct()
+    {
+        $this->usersInscrits = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(int $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getDateCloture(): ?\DateTimeInterface
+    {
+        return $this->dateCloture;
+    }
+
+    public function setDateCloture(\DateTimeInterface $dateCloture): self
+    {
+        $this->dateCloture = $dateCloture;
+
+        return $this;
+    }
+
+    public function getDescriptioninfos(): ?string
+    {
+        return $this->descriptioninfos;
+    }
+
+    public function setDescriptioninfos(string $descriptioninfos): self
+    {
+        $this->descriptioninfos = $descriptioninfos;
+
+        return $this;
+    }
+
+    public function getNbInscriptionsMax(): ?int
+    {
+        return $this->nbInscriptionsMax;
+    }
+
+    public function setNbInscriptionsMax(int $nbInscriptionsMax): self
+    {
+        $this->nbInscriptionsMax = $nbInscriptionsMax;
+
+        return $this;
+    }
+
+    public function getUrlPhoto(): ?string
+    {
+        return $this->urlPhoto;
+    }
+
+    public function setUrlPhoto(?string $urlPhoto): self
+    {
+        $this->urlPhoto = $urlPhoto;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?User
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?User $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getEtat(): ?Etat
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(?Etat $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getLieu(): ?Lieu
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(?Lieu $lieu): self
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getSite(): ?Site
+    {
+        return $this->site;
+    }
+
+    public function setSite(?Site $site): self
+    {
+        $this->site = $site;
+
+        return $this;
+    }
+
+    public function getUsersInscrits(): Collection
+    {
+        return $this->usersInscrits;
+    }
+
+    public function addUsersInscrit(User $usersInscrit): self
+    {
+        if (!$this->usersInscrits->contains($usersInscrit)) {
+            $this->usersInscrits[] = $usersInscrit;
+            $usersInscrit->addInscription($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUsersInscrit(User $usersInscrit): self
+    {
+        if ($this->usersInscrits->removeElement($usersInscrit)) {
+            $usersInscrit->removeInscription($this);
+        }
+
+        return $this;
+    }
+
+    public function getMotifAnnulation(): ?string
+    {
+        return $this->motifAnnulation;
+    }
+
+    public function setMotifAnnulation(?string $motifAnnulation): self
+    {
+        $this->motifAnnulation = $motifAnnulation;
+
+        return $this;
+    }
+}
