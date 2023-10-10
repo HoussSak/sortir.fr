42e583cd0e2bb6088922f33c9e3c3647320e6313
HoussSak <99645587+HoussSak@users.noreply.github.com>
2023-10-10 11:27:04 +0200

    adding roles

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,6 +30,12 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            if($form->get('administrateur')->getData() === true) {
+                $user->setRoles(['ROLE_ADMIN']);
+            } else {
+                $user->setRoles(['ROLE_USER']);
+            }
+
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
