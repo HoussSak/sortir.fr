1a3d0e0666508d4927c524dcc1928fc0b63e0916
sylvaincoulay <museum@netcourrier.com>
2023-10-11 16:06:38 +0200

    add fixtures

--- src/DataFixtures/LieuFixtures.php
+++ src/DataFixtures/LieuFixtures.php
@@ -5,28 +5,55 @@ namespace App\DataFixtures;
 use App\Entity\Lieu;
 use App\Entity\Ville;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class LieuFixtures extends  Fixture
+class LieuFixtures extends  Fixture 
 {
+
     public function load(ObjectManager $manager)
     {
+        $niort = new Ville();
+        $niort->setNom('Niort');
+        $niort->setCodePostal('79000');
+        $manager->persist($niort);
+
+        $nantes = new Ville();
+        $nantes->setNom('Nantes');
+        $nantes->setCodePostal('44000');
+        $manager->persist($nantes);
+
+        $quimper = new Ville();
+        $quimper->setNom('Quimper');
+        $quimper->setCodePostal('29000');
+        $manager->persist($quimper);
+
+        $manager->flush();
+
+
         $villes = $manager->getRepository(Ville::class)->findAll();
 
         $lieuxData = [
             [
-                'nom' => 'Lieu 1',
-                'rue' => 'Rue 1',
+                'nom' => 'Les Remparts',
+                'rue' => 'Place de la Brèche',
                 'latitude' => 48.858844,
                 'longitude' => 2.294351,
                 'ville' => $villes[0],
             ],
             [
-                'nom' => 'Lieu 2',
-                'rue' => 'Rue 2',
+                'nom' => 'Bowling',
+                'rue' => 'rue de Nantes',
                 'latitude' => 51.507222,
                 'longitude' => -0.12750,
                 'ville' => $villes[1],
+            ],
+            [
+                'nom' => 'Cinéma',
+                'rue' => 'rue de Quimper',
+                'latitude' => 51.507222,
+                'longitude' => -0.12750,
+                'ville' => $villes[2],
             ],
         ];
 
